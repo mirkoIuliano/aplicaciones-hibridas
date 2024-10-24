93598007509acbae6078db2bb5e157fa8b37a5ff
mirko iuliano <federico.iuliano@davinci.edu.ar>
2024-10-24 16:50:51 -0300

    creación de proyecto para actividad 08

--- clase-21/src/App.jsx
+++ clase-21/src/App.jsx
@@ -2,6 +2,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './Card';
+import User from './User';
 
 function App() {
 let nombre = "mirko";
@@ -9,10 +10,17 @@ let edad = 21;
 const curso = ['HTML', 'JS', 'React'];
 let titulo = <h2>Esto es un título h2</h2>
 // se puede guardar etiquetas html dentro de una vairable gracias a JSX
+
+const userData = {
+  name: 'Juan',
+  email: 'juan@gmail.com'
+}
+
   return (
     <>
     <div>
       <h1 className='verde'> Aplicaciones Híbridas Clase 20 </h1>
+      <User name={userData.name} email={userData.email} />
       {/* así usamos el componente y pasamos propiedades */}
       <Card producto="Termo" precio={40000}/>
       {/* cuando enviamos una propiedad como un numero, objeto, booleaono, etc lo mandamos con {} y sin "" */}
