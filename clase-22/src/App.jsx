01e3c937b5c092eaa41c5cbfca214e55df829324
mirko iuliano <federico.iuliano@davinci.edu.ar>
2024-10-28 00:07:26 -0300

    clase 22 terminada. Vimos Estado con useState, el Evento onClick, operativo ternario para renderizado condicional y el uso de map() para renderiado de lista

--- clase-22/src/App.jsx
+++ clase-22/src/App.jsx
@@ -1,60 +1,88 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './Card';
-import User from './User';
-// importamos useState para poder renderizar el contenido que traemos del fetch
+
 import { useState } from 'react';
-// importamos useEffect que sirve para que se renderice la página una sola vez, en vez de 2 como lo hace react por defecto
-import { useEffect } from 'react';
-
-
 
 function App() {
-  const [ users, setUsers ] = useState([]);
+/* 
+EXPLICACIÓN DE useState 
+  let estado = useState(false); // Retorna un array que dentro tiene: [ valor del estado, función]
+  let logueado = estado[0]; // En 'logueado' estaríamos guardando el valor del estado
+  let setLogueado = estado [1]; // En 'setLogueado' estaríamos guardando la función
+*/
+// En vez de hacerlo como mostré antes lo desestructurizamos
+let [logueado, setLogueado] = useState(false)
 
-  useEffect( () => {
-    const getUsers = async () => {
-      const endPoint = 'http://127.0.0.1:3000/api/usuarios';
-      const resp = await fetch(endPoint);
-      const users = await resp.json();
-      setUsers(users.usuarios); // la verdad todavía no enteindo esto de setUsers, useEffects, etc. Pero sobre 'users.usuarios': el 'usuarios' viene de que dentro de mi objeto users (que es la respuesta del fetch que hicimos) tengo dos propiedades: mensajes y usuarios. 'usuarios' tiene los array con los datos 
-      console.log(users);
-    }
-    getUsers();
-  }, [])
-
+  let mensaje = logueado == true ? 'Bienvenido' : 'Iniciar sesión'
   let nombre = "mirko";
   let edad = 21;
-  const curso = ['HTML', 'JS', 'React'];
-  let titulo = <h2>Esto es un título h2</h2>
-  // se puede guardar etiquetas html dentro de una vairable gracias a JSX
-  const userData = {
-    name: 'Juan',
-    email: 'juan@gmail.com'
+  const productos = [
+    {id: 1, nombre: 'Mate', precio: 2000 },
+    {id: 2, nombre: 'Galletitas', precio: 740 },
+    {id: 3, nombre: 'Bizcochitos', precio: 900 },
+  ];
+  let titulo = <h2>App Hibrid</h2>
+
+  const Login = () => {
+    alert ('Iniciaste Sesión')
+    // modificamos el estado de 'logueado' con la función setLogueado
+    setLogueado(true)
+  }
+
+  const Logout = () => {
+    alert ('Cerraste Sesión')
+    // modificamos el estado de 'logueado' con la función setLogueado
+    setLogueado(false)
   }
 
   return (
     <div>
         <h1 className='verde'> Aplicaciones Híbridas</h1>
-        {
-          users.map( user => (
-            <User 
-              name={user.name} 
-              email={user.email} 
-            />
-          ))
-        }
+
+        {titulo}
+        <h3>{mensaje}</h3>
+        <p>{nombre.toUpperCase()} </p>
+
+        <button type='button' onClick={Login}>
+          Iniciar Sesión
+        </button>
+        <button type='button' onClick={Logout}>
+          Cerrar Sesión
+        </button>
+
+        <hr />
         
-        {/* así usamos el componente y pasamos propiedades */}
-        <Card producto="Termo" precio={40000}/>
-        {/* cuando enviamos una propiedad como un numero, objeto, booleaono, etc lo mandamos con {} y sin "" */}
-        <Card producto="Mate" precio={8000}/>
-        {/* se imprimen variable JS con {} */}
-        <p>Mi nombre es {nombre.toUpperCase()} </p>
-        {/* se pueden hacer operaciones */}
-        <p> Mi edad es {edad + 2} </p>
-        <p> Hoy estamos viendo {curso[2]} </p>
+
+        {/* EJEMPLO DE USO DE OPERADOR TERNARIO Y RENDERIZADO CONDICIONAL */}
+        <> {/* SI EN LAS RESPUESTAS VAMOS A PONER ETIQUETAS HTML TENEMOS QUE ENCERRAR EL OPERADOR TERNARIO DENTRO DE UN DIV O DENTRO DE UN <> </> */}
+          {
+            logueado == true ? (
+              <div className='row'>
+
+                {/* EJEMPLO DE RENDERIZADO DE LISTA */}
+                <div className='row'> {/* Este div no es necesario, pero lo ponemos para que queden los producot suno al lado del otro */}
+                  {
+                    productos.map(producto => {
+                      // return <li> {producto.nombre}  | $ {producto.precio} </li>
+                      return <Card key={producto.id} producto={producto.nombre} precio={producto.precio} />
+                    })
+                  }
+                  {/* algo importante sobre el map() es que después del => podemos poner llaves {} o paréntesis ()
+                  La diferencia es que si usamos llaves {} vamos a necesitar poner el return
+                  Y si usamos paréntesis el return no lo necesitamos. Ejemplo con paréntesis:
+                  productos.map(producto => (
+                      <Card key={producto.id} producto={producto.nombre} precio={producto.precio} />
+                      ))
+                  */}
+                </div>
+
+              </div>
+            ) : ( <h4>Iniciar sesión <a href='#' onClick={Login}>Login</a></h4> ) 
+          }
+        </>
+        
+        
+
 
     </div>
   )
