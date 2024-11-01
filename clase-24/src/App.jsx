796785babf87bc8d68c5ab35ecc33e3ae880ce41
mirko iuliano <federico.iuliano@davinci.edu.ar>
2024-11-01 10:38:27 -0300

    codigo de clase 24 ya terminado

--- clase-24/src/App.jsx
+++ clase-24/src/App.jsx
@@ -1,93 +1,53 @@
 import './App.css'
-import Card from './components/Card'
-import Button from './components/Button';
-import ProductsContainer from './components/ProductsContainer';
 
-import { useState } from 'react';
+
+import { Routes, Route, NavLink, Link } from 'react-router-dom'; // con esto ya tengo todo el sistema de rutas y las rutas singular
+
+// Importamos las vistas
+import Home from './views/Home';
+import Contact from './views/Contacts';
+import Details from './views/Details';
+import Login from './views/Login';
+import NotFound from './views/NotFound';
 
 function App() {
-/* 
-EXPLICACIÓN DE useState 
-  let estado = useState(false); // Retorna un array que dentro tiene: [ valor del estado, función]
-  let logueado = estado[0]; // En 'logueado' estaríamos guardando el valor del estado
-  let setLogueado = estado [1]; // En 'setLogueado' estaríamos guardando la función
-*/
-// En vez de hacerlo como mostré antes lo desestructurizamos
-let [logueado, setLogueado] = useState(false)
 
-  let mensaje = logueado == true ? 'Bienvenido' : 'Iniciar sesión'
-  let nombre = "mirko";
-  let edad = 21;
-  const productos = [
-    {id: 1, nombre: 'Mate', precio: 2000 },
-    {id: 2, nombre: 'Galletitas', precio: 740 },
-    {id: 3, nombre: 'Bizcochitos', precio: 900 },
-  ];
-  let titulo = <h2>App Hibrid</h2>
-
-  const Login = () => {
-    alert ('Iniciaste Sesión')
-    // modificamos el estado de 'logueado' con la función setLogueado
-    setLogueado(true)
-  }
-
-  const Logout = () => {
-    alert ('Cerraste Sesión')
-    // modificamos el estado de 'logueado' con la función setLogueado
-    setLogueado(false)
-  }
 
   return (
     <div>
         <h1 className='verde'> Aplicaciones Híbridas</h1>
-
-        {titulo}
-        <h3>{mensaje}</h3>
-        <Button text="Hola" color="red"></Button>
-        <p>{nombre.toUpperCase()} </p>
-
-        <button type='button' onClick={Login}>
-          Iniciar Sesión
-        </button>
-        <button type='button' onClick={Logout}>
-          Cerrar Sesión
-        </button>
-
         <hr />
+
+        <Link to="login" >Login</Link>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Inicio</NavLink>
+            </li>
+            <li>
+              <NavLink to="detalles">Detalles</NavLink>
+            </li>
+            <li>
+              <NavLink to="contacto">Contacto</NavLink>
+            </li>
+            <li>
+              <NavLink to="iniciar-sesion">Login</NavLink>
+            </li>
+          </ul>
+        </nav>
+        {/* Este es el área donde se van a mostrar los componentes */}
+        <Routes>
+          <Route path="/" element= { <Home/> } />
+          <Route path="/contacto" element= { <Contact/> } />
+          <Route path="/detalles/:id" element= { <Details/> } />
+          <Route path="/iniciar-sesion" element= { <Login/> } />
+          <Route path="*" element= { <NotFound/> } />
+        </Routes>
+
+
+
         
         
-
-        {/* EJEMPLO DE USO DE OPERADOR TERNARIO Y RENDERIZADO CONDICIONAL */}
-        <> {/* SI EN LAS RESPUESTAS VAMOS A PONER ETIQUETAS HTML TENEMOS QUE ENCERRAR EL OPERADOR TERNARIO DENTRO DE UN DIV O DENTRO DE UN <> </> */}
-          {
-            logueado == true ? (
-              <div className='row'>
-
-                <ProductsContainer>
-                  {/* EJEMPLO DE RENDERIZADO DE LISTA */}
-                  <div className='row'> {/* Este div no es necesario, pero lo ponemos para que queden los producot suno al lado del otro */}
-                    {
-                      productos.map(producto => {
-                        // return <li> {producto.nombre}  | $ {producto.precio} </li>
-                        return <Card key={producto.id} producto={producto.nombre} precio={producto.precio} />
-                      })
-                    }
-                    {/* algo importante sobre el map() es que después del => podemos poner llaves {} o paréntesis ()
-                    La diferencia es que si usamos llaves {} vamos a necesitar poner el return
-                    Y si usamos paréntesis el return no lo necesitamos. Ejemplo con paréntesis:
-                    productos.map(producto => (
-                        <Card key={producto.id} producto={producto.nombre} precio={producto.precio} />
-                        ))
-                    */}
-                  </div>
-                </ProductsContainer>
-
-                
-
-              </div>
-            ) : ( <h4>Iniciar sesión <a href='#' onClick={Login}>Login</a></h4> ) 
-          }
-        </>
         
         
 
--- clase-24/src/components/Card.jsx
+++ clase-24/src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import { Link } from "react-router-dom";
 
 /* 
 Se puede hacer la función así:
@@ -8,15 +9,16 @@ function Card = () => {
 }
 */
 /* pero la manera mas moderna y la que se usa es esta: */
-function Card (props) {
-    return (
-        <div className="card">
-            <h3>{props.producto}</h3>
-            <hr />
-            <p>${props.precio + 100}</p>
-            <Button text="Agregar" color="green"></Button>
-        </div>
-    )
+function Card(props) {
+  return (
+    <div className="card">
+      <h3>{props.producto}</h3>
+      <hr />
+      <p>${props.precio + 100}</p>
+      <Button text="Agregar" color="green"></Button>
+      <Link to={`/detalles/${props.id}`}>Detalle</Link>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
